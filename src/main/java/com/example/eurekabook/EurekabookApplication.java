db2db6312093b1ad18f7bbc0ecc9d66b0992b850
anxgh-n <anaghmanoj@gmail.com>
2024-10-22 14:43:14 +0530

    new eureka

---
+++ src/main/java/com/example/eurekabook/EurekabookApplication.java
@@ -0,0 +1,13 @@
+package com.example.eurekabook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekabookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekabookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/eurekabook/EurekabookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eurekabook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekabookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
